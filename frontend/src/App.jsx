546c79f417e69e98709a8909e1228dc008fb43e4
dinesh kumar <cdk421413@gmail.com>
2025-05-21 16:55:16 +0530

    Initial Commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router';
+import { Routes, Route } from 'react-router'; 
 import HomePage from './pages/HomePage';
 import CreateNotePage from './pages/CreateNotePage';
 import NoteDetailPage from './pages/NoteDetailPage';
@@ -6,15 +6,22 @@ import NoteDetailPage from './pages/NoteDetailPage';
 const App = () => {
   return (
     <div className='relative h-full w-full bg-slate-950'>
-      <div class="absolute bottom-0 left-0 right-0 top-0 bg-[radial-gradient(circle_500px_at_50%_300px,#3e3e3e,transparent)]"></div>
-      {/* <div class="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_60%,#ff8f00_100%)]"></div> */}
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/create" element={<CreateNotePage />} />
-        <Route path="/note/:id" element={<NoteDetailPage />} />
-      </Routes>
-    </div>
-  )
-}
+      {/* Background Layer */}
+      <div className="absolute inset-0 z-0 bg-[radial-gradient(circle_500px_at_50%_300px,#3e3e3e,transparent)]"></div>
 
-export default App
+      {/* Optional alternative gradient */}
+      {/* <div className="absolute inset-0 z-0 h-full w-full items-center px-5 py-24 bg-[radial-gradient(125%_125%_at_50%_10%,#000_60%,#ff8f00_100%)]"></div> */}
+
+      {/* Routes Layer */}
+      <div className="relative z-10">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/create" element={<CreateNotePage />} />
+          <Route path="/note/:id" element={<NoteDetailPage />} />
+        </Routes>
+      </div>
+    </div>
+  );
+};
+
+export default App;
