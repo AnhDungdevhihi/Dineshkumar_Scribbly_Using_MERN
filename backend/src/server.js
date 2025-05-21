91f0b4e32d615cc5afc4648a9e70542802342327
dinesh kumar <cdk421413@gmail.com>
2025-05-21 16:35:10 +0530

    Initial Commit

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,20 +4,34 @@ import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
 import rateLimiter from "./middleware/rateLimiter.js";
 import cors from "cors";
+import path from "path"; // for deploy
 
 const app = express();
 const PORT = process.env.PORT || 5001
 
-dotenv.config();
+const __dirname = path.resolve() // for deploy
 
-app.use(cors({
-    origin : "http://localhost:5173",
-}));
+dotenv.config();
+if (process.env.NODE_ENV !== "production") {
+    app.use(cors({
+        origin: "http://localhost:5173",
+    }));
+}
+
 
 app.use(express.json()); // middleware
 app.use(rateLimiter);
 
 app.use("/api/notes", notesRoutes);
+
+// for deploy
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, '../frontend/dist')))
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"))
+    })
+}
 
 connectDB().then(() => {
     app.listen(PORT, () => {
